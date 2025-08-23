d290653044e29c855bd308af8d6029235a3991e3
ynbflmk <stankovichvadim@gmail.com>
2025-08-23 12:03:55 +0300

    Squashed commit of the following:
    commit 3f6f4bdfbf4f0b0840d9cecfff685ae877d195e0
    Merge: 7161e6e 6647a68
    Author: ynbflmk <stankovichvadim@gmail.com>
    Date:   Sat Aug 23 12:03:32 2025 +0300
    
        Merge branch 'master' into vadim
    
    commit 7161e6ec1647a577760da34a54d59d0603206fe5
    Author: ynbflmk <stankovichvadim@gmail.com>
    Date:   Sat Aug 23 12:02:26 2025 +0300
    
        speed fix
    
    commit 1e1b74bc21e197d2d874996239471586114fc0c3
    Author: ynbflmk <stankovichvadim@gmail.com>
    Date:   Fri Aug 22 19:44:51 2025 +0300
    
        movement changed
    
    commit 8e837ed1fd683ef7b78030f3bcd9fad376a978d4
    Author: ynbflmk <stankovichvadim@gmail.com>
    Date:   Fri Aug 22 18:54:19 2025 +0300
    
        added level and the movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,38 +1,25 @@
 extends CharacterBody2D
 
-const BASE_SPEED = 100.0  # base speed on flat surface
+
+const SPEED = 130.0
 const JUMP_VELOCITY = -400.0
 
+
 func _physics_process(delta: float) -> void:
-	# Add gravity
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Jump
+	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get input (left/right)
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	var move_speed = BASE_SPEED
-
-	# Adjust speed smoothly depending on slope
-	if is_on_floor():
-		var collision = get_last_slide_collision()
-		if collision:
-			var normal = collision.get_normal()
-			# Slope angle in degrees (0 = flat, 90 = vertical)
-			var slope_angle = rad_to_deg(acos(normal.dot(Vector2.UP)))
-
-			# Smooth speed adjustment:
-			# flat road: slower, steep slope: slowest, moderate slope: normal
-			var slope_factor = clamp(1.0 - slope_angle / 90.0, 0.2, 0.8)
-			move_speed *= slope_factor
-
-	# Movement
 	if direction:
-		velocity.x = direction * move_speed
+		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, BASE_SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
